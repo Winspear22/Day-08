e649c7dfce21f2d6f4276dad24649f8b24c869f5
Winspear22 <adnen.aloui2@hotmail.com>
2025-10-03 04:45:42 +0200

    PHP-08 -- ex01 : progrès.

--- ex/src/Controller/UserController.php
+++ ex/src/Controller/UserController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/login', name: 'login_action', methods: ['POST'])]
+    public function loginAction(Request $request): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        // Cas où il manque un champ !
+        if (!$username || !$password)
+        {
+            return new JsonResponse(
+                ['error' => 'Champs manquant'],
+                400 // Bad Request
+            );
+        }
+
+        // Ici tu mets ta vraie logique de login plus tard (user/pass test)
+        // Pour l’instant, juste un message
+        return new JsonResponse([
+            'message' => "Bienvenue $username !"
         ]);
     }
 }
